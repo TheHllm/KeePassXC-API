290b1aaee510396d597d6559d8f1c7f2cc9da0b1
TheHllm <hllmollenkopf@gmail.com>
2020-12-26 23:09:13 +0100

    Add example

---
+++ Example/Program.cs
@@ -0,0 +1,42 @@
+﻿using KeePassXC_API;
+using System;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("\t -lock \t lock the database");
+                Console.WriteLine("\t -logins [url] \t get all logins for a url");
+                Console.WriteLine("\t -generate \t generates a password");
+                Environment.Exit(0);
+            }
+
+            KeepassXCApi api = new();
+
+            switch (args[0])
+            {
+                case "-lock":
+                    await api.LockDatabase();
+                    break;
+
+                case "-logins":
+                    AccountInformation[] logins = await api.GetLogins(args[1]);
+                    foreach(var login in logins)
+                    {
+                        Console.WriteLine("{0}\n{1}", login.Username, login.Password);
+                    }
+                    break;
+
+                case "-generate":
+                    Console.WriteLine(await api.GeneratePassword());
+                    break;
+            }
+        }
+    }
+}
---
+++ KeePassXC-API/MessaginProvider.cs
@@ -0,0 +1,20 @@
+﻿using NativeMessaging;
+using System;
+using System.Threading.Tasks;
+
+namespace KeePassXC_API
+{
+    class MessaginProvider : IDisposable
+    {
+        private Task<Client> _client = Client.GetByName("org.keepassxc.keepassxc_browser");
+        private Client Client { get { if (!_client.IsCompleted) { _client.Wait(); } return _client.Result; } }
+
+        public const int KeySize = 24;
+
+        public MessaginProvider(){}
+        void IDisposable.Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
